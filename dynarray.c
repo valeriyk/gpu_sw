76644ede669d0f6558018d4f7d08a67074c432a2
Valeriy Kazantsev <valeriy.kazantsev@gmail.com>
2016-09-05 19:47:17 +0300

    added dynarray files to version control

---
+++ dynarray.c
@@ -0,0 +1,33 @@
+#include "dynarray.h"
+
+
+DynArray * dyn_array_create (size_t elem_size, size_t initial_max) {
+	DynArray *a = (DynArray*) malloc(sizeof(DynArray));
+	a->max = initial_max;
+	a->data = (DynArrayItem*) calloc(initial_max, sizeof(DynArrayItem));
+	a->end = 0;
+	a->elem_size = elem_size;
+	a->expand_rate = 256;
+	
+	return a;
+}
+
+int dyn_array_push (DynArray *a, DynArrayItem *el) {
+	a->data[a->end] = *el;
+	a->end++;
+	if (a->end >= a->max) return dyn_array_expand(a);
+	else return 0;
+}
+
+int dyn_array_expand  (DynArray *a) {
+	a->max = a->max + a->expand_rate;
+	a->data = (DynArrayItem*) realloc(a->data, a->max * sizeof (DynArrayItem));
+	return 0;
+}
+
+void dyn_array_destroy (DynArray *a) {
+	if (a) {
+		if (a->data) free(a->data);
+		free(a);
+	}
+}
---
+++ dynarray.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+#include <stdlib.h>
+
+typedef union DynArrayItem {
+	size_t i;
+	float  f;
+} DynArrayItem;
+
+typedef struct DynArray {
+	int end;
+	int max;
+	size_t elem_size;
+	size_t expand_rate;
+	DynArrayItem *data;
+} DynArray;
+
+
+
+DynArray * dyn_array_create (size_t elem_size, size_t initial_max);
+
+int    dyn_array_push    (DynArray *a, DynArrayItem *el);
+int    dyn_array_expand  (DynArray *a);
+void   dyn_array_destroy (DynArray *a);
+
+static inline void* dyn_array_get (DynArray *a, int i) {
+	return &(a->data[i]);
+}
