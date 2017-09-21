835c1cb99612ef08a253b54dc32f19f032a10a08
Valeriy Kazantsev <valeriy.kazantsev@gmail.com>
2017-09-22 02:24:29 +0300

    added user shaders back

---
+++ fw/host/shader_gouraud.c
@@ -0,0 +1,106 @@
+#include "shader_gouraud.h"
+#include "gl.h"
+
+#include "geometry_fixpt.h"
+
+//#include <fixmath.h>
+
+#include <math.h>
+
+
+
+// Layout of Varying words:
+//     
+// 0   intensity
+// 1   texture.u
+// 2   texture.v
+// ....
+
+Float4 vshader_gouraud (Object *obj, size_t face_idx, size_t vtx_idx, Varying *vry, gpu_cfg_t *cfg) {
+	
+	// transform 3d coords of the vertex to homogenous clip coords
+	Float3 model   = wfobj_get_vtx_coords (obj->wfobj, face_idx, vtx_idx);
+	Float4 model4d = Float3_Float4_conv (&model, 1);
+	Float4 clip    = fmat4_Float4_mult (&(obj->mvp), &model4d); // model -> world -> eye -> clip
+	
+	// transform the normal vector to the vertex
+	Float3 norm       = wfobj_get_norm_coords    (obj->wfobj, face_idx, vtx_idx);
+	Float4 norm4d     = Float3_Float4_conv  (&norm, 0);
+	Float4 norm4d_eye = fmat4_Float4_mult (&(obj->mvit), &norm4d);
+	Float3 norm_eye   = Float4_Float3_vect_conv (&norm4d_eye); // normal is a vector, hence W = 0 and I don't care about it
+	Float3_normalize (&norm_eye);
+	float diff_intensity = -Float3_Float3_smult (&norm_eye, &(cfg->lights_arr[0].eye));// TBD, why [0]?
+	
+	assert (diff_intensity <=  1.0f);
+	assert (diff_intensity >= -1.0f);
+	
+	varying_fifo_push_float (vry, diff_intensity);
+	
+	
+	// extract the texture UV coordinates of the vertex
+	if (obj->texture != NULL) {
+		Float2 texture = wfobj_get_texture_coords (obj->wfobj, face_idx, vtx_idx);
+		
+		assert (texture.as_struct.u >= 0.0f);
+		assert (texture.as_struct.v >= 0.0f);
+		assert (texture.as_struct.u <= 1.0f);
+		assert (texture.as_struct.v <= 1.0f);
+		
+		texture.as_struct.u *= (float) obj->texture->w;
+		texture.as_struct.v *= (float) obj->texture->h;
+	
+		varying_fifo_push_Float2 (vry, &texture);
+	}
+			
+	return clip;		
+}
+
+bool pshader_gouraud (Object *obj, Varying *vry, Light *lights_arr, uint32_t screen_width, uint32_t screen_height, pixel_color_t *color) {
+	
+	float intensity = varying_fifo_pop_float (vry);
+	//intensity = 0.9;
+	assert (intensity <=  1.0f);
+	assert (intensity >= -1.0f);
+	if ((intensity >  1.0f) || (intensity < -1.0f)) {
+		return false;
+	}
+		
+	//
+	// If texture is not provided, use gray color
+	//
+	pixel_color_t pix;
+	if (obj->texture == NULL) {
+		pix = set_color (128, 128, 128, 0);
+	}
+	else {	
+
+		Float2 text   = varying_fifo_pop_Float2 (vry);
+		
+		assert (text.as_struct.u >= 0);
+		assert (text.as_struct.v >= 0);
+		
+		size_t uu = (size_t) text.as_struct.u;
+		size_t vv = (size_t) text.as_struct.v;
+		assert (uu < obj->texture->w);
+		assert (vv < obj->texture->h);
+		
+		pix = get_pixel_color_from_bitmap (obj->texture, uu, vv);
+	}
+	
+	float intensity_treshold = 0.2;
+	if (intensity < intensity_treshold) {
+		intensity = intensity_treshold;
+	}
+		
+	int r = pix.r * intensity + 5;
+	int g = pix.g * intensity + 5;
+	int b = pix.b * intensity + 5;
+		
+	if (r > 255) r = 255;
+	if (g > 255) g = 255;
+	if (b > 255) b = 255;
+	
+	*color = set_color (r, g, b, 0);
+	//*color = set_color (128, 128, 0, 0);
+	return true;
+}
---
+++ fw/host/shader_normalmap.c
@@ -0,0 +1,103 @@
+/*#include "shader_normalmap.h"
+#include "gl.h"
+
+#include <math.h>
+
+
+
+// declare the following here instead of the header to make these variables local:
+Float3 NM_VARYING_U;
+Float3 NM_VARYING_V;
+
+// returns normalized device coordinates (NDC)
+Float4 nm_vertex_shader (WFobj *obj, int face_idx, int vtx_idx, fmat4 *mvpv) {
+	
+	if (NM_VSHADER_DEBUG) {
+		printf ("\tcall nm_vertex_shader()\n");
+	}
+	
+	// transform 3d coords of the vertex to homogenous clip coords
+	Float3 vtx3d = wfobj_get_vtx_coords (obj, face_idx, vtx_idx);
+	Float4 mc = Float3_Float4_conv (&vtx3d, 1);
+	Float4 vtx4d = fmat4_Float4_mult (mvpv, &mc);
+	
+	// extract the texture UV coordinates of the vertex
+	if (obj->texture != NULL) {
+		Float2 vtx_uv = wfobj_get_texture_coords (obj, face_idx, vtx_idx);
+		NM_VARYING_U.as_array[vtx_idx] = vtx_uv.as_struct.u;
+		NM_VARYING_V.as_array[vtx_idx] = vtx_uv.as_struct.v;
+	};
+	
+	return vtx4d;
+}
+
+bool nm_pixel_shader (WFobj *obj, Float3 *barw, pixel_color_t *color) {
+	
+	if (NM_PSHADER_DEBUG) {
+		printf ("\t\tcall nm_pixel_shader()\n");
+	}
+	
+	int uu = (int) Float3_Float3_smult (&NM_VARYING_U, barw);
+	int vv = (int) Float3_Float3_smult (&NM_VARYING_V, barw);
+	
+	pixel_color_t pix;
+	if (obj->texture != NULL) {
+		if (uu >= obj->texture->w || vv >= obj->texture->h) return false;
+		wfobj_get_rgb_from_texture (obj, uu, vv, &pix.r, &pix.g, &pix.b);
+	}
+	else {
+		pix = set_color (128, 128, 128, 0);
+	}
+	
+	Float3 normal;
+	Float3 nm3 = wfobj_get_normal_from_map (obj, uu, vv);
+	Float4 nm4 = Float3_Float4_conv (&nm3, 0);
+	Float4 tmp = fmat4_Float4_mult (&UNIFORM_MIT, &nm4);
+	normal     = Float4_Float3_vect_conv (&tmp);
+	Float3_normalize (&normal);
+	float diff_intensity = -Float3_Float3_smult (&normal, &(LIGHTS[0].eye));
+	
+	float spec_intensity = 0;	
+	if (obj->specularmap != NULL) {
+		float nl = diff_intensity; // this is float3_float3_smult (&normal, &UNIFORM_LIGHT), computed above
+		Float3 nnl2 = Float3_float_mult (&normal, nl * 2.0f);
+		Float3 r    = Float3_Float3_add (&nnl2, &(LIGHTS[0].eye));
+		Float3_normalize (&r);
+		
+		int spec_factor = wfobj_get_specularity_from_map (obj, uu, vv);
+		spec_intensity = (r.as_struct.z < 0) ? 0 : powf (r.as_struct.z, spec_factor);
+		
+		if (NM_PSHADER_DEBUG) {
+			if (spec_intensity >= 0.5) {
+				printf ("n=(%f;%f;%f) ", normal.as_struct.x, normal.as_struct.y, normal.as_struct.z);
+				printf ("nl=%f ", nl);
+				printf ("nnl2=(%f;%f;%f) ", nnl2.as_struct.x, nnl2.as_struct.y, nnl2.as_struct.z);
+				printf ("r=(%f;%f;%f) ", r.as_struct.x, r.as_struct.y, r.as_struct.z);
+				printf ("spec_f=%d spec_i=%f\n", spec_factor, spec_intensity);
+			}
+		}
+	}
+	
+	float intensity = 1.0f * diff_intensity + 0.6f * spec_intensity;
+	
+	if (NM_PSHADER_DEBUG) {
+		if (spec_intensity > 0.5) printf ("spec%f diff%f total%f\n", spec_intensity, diff_intensity, intensity);
+	}
+	
+	if (intensity < 0) return false;
+	int r = pix.r * intensity + 5;
+	int g = pix.g * intensity + 5;
+	int b = pix.b * intensity + 5;
+		
+	if (r > 255) r = 255;
+	if (g > 255) g = 255;
+	if (b > 255) b = 255;
+	
+	if (NM_PSHADER_DEBUG) {
+		if (spec_intensity > 0.5) printf ("r%d g%d b%d\t", r, g, b);
+	}
+	
+	*color = set_color (r, g, b, 0);
+	return true;
+}
+*/
